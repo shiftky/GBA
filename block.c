300888ee66c6c787c05adde3f06a56d0882127fc
shiftky <shiftky@gmail.com>
2014-05-29 02:41:20 +0900

    ブロックの表示位置を修正

--- block.c
+++ block.c
@@ -8,6 +8,7 @@
 #define BLOCK_COLS    8
 #define BLOCK_ROWS    5
 #define BLOCK_TOP     10
+#define BLOCK_LEFT    20
 #define BLOCK_WIDTH   25
 #define BLOCK_HEIGHT  10
 
@@ -21,10 +22,10 @@ static void init_block(void)
   num_blocks = BLOCK_COLS * BLOCK_ROWS;
   for ( i=0; i<BLOCK_COLS; i++ ) {
     for ( j=0; j<BLOCK_ROWS; j++ ) {
-      boxes[i][j].x = i*BLOCK_WIDTH;
+      boxes[i][j].x = 20 + i*BLOCK_WIDTH;
       boxes[i][j].y = BLOCK_TOP + j*BLOCK_HEIGHT;
-      boxes[i][j].width = BLOCK_WIDTH;
-      boxes[i][j].height = BLOCK_HEIGHT;
+      boxes[i][j].width = BLOCK_WIDTH - 1;
+      boxes[i][j].height = BLOCK_HEIGHT - 1;
       flags[i][j] = 1;
       draw_box(&boxes[i][j], boxes[i][j].x, boxes[i][j].y, COLOR_WHITE);
     }
@@ -33,7 +34,7 @@ static void init_block(void)
 
 static int hit(int x, int y)
 {
-  int i = x/BLOCK_WIDTH, j = (y-BLOCK_TOP)/BLOCK_HEIGHT;
+  int i = (x-BLOCK_LEFT)/BLOCK_WIDTH, j = (y-BLOCK_TOP)/BLOCK_HEIGHT;
   if ( i < 0 || j < 0 || i >= BLOCK_COLS || j >= BLOCK_ROWS ) {
     return -1;
   }
@@ -42,7 +43,7 @@ static int hit(int x, int y)
 
 void remove_block(int x, int y, struct box *ball)
 {
-  int i = x/BLOCK_WIDTH, j = (y-BLOCK_TOP)/BLOCK_HEIGHT;
+  int i = (x-BLOCK_LEFT)/BLOCK_WIDTH, j = (y-BLOCK_TOP)/BLOCK_HEIGHT;
   if ( cross(&boxes[i][j], ball) == 1 ) {
     draw_box(&boxes[i][j], boxes[i][j].x, boxes[i][j].y, COLOR_BLACK);
     flags[i][j] = 0;
@@ -53,7 +54,6 @@ void remove_block(int x, int y, struct box *ball)
 void block_step(void)
 {
   struct box *ball;
-  int x, y, w, h;
   switch ( game_get_state() ) {
     case START:
       init_block();
